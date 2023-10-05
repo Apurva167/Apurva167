9567f5cb6c9ab23c4b2b954e486b18abd4923ffd
Apurva167 <146975358+Apurva167@users.noreply.github.com>
2023-10-05 19:00:31 +0530

    Update SmartHomeSystem.java

--- src/com/smart_home_system/core/SmartHomeSystem.java
+++ src/com/smart_home_system/core/SmartHomeSystem.java
@@ -4,8 +4,10 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,18 +18,34 @@ import com.smart_home_system.tasks.AutomatedTrigger;
 import com.smart_home_system.tasks.ScheduledTask;
 
 public class SmartHomeSystem {
-    private Map<Integer, Device> devices;
+	private Map<Integer, Device> devices;
     private List<ScheduledTask> scheduledTasks;
     private List<AutomatedTrigger> automatedTriggers;
-
+    private Set<Integer> ids;
+    private Timer timer;
+    public int presentInterval(int targetTriggerId) {
+    	for(AutomatedTrigger x:automatedTriggers) {
+    		if(x.getTriggerId()==targetTriggerId) {
+    			return x.getIntervalInSeconds();
+    		}
+    	}
+    	return 0;//if not found
+    }
     public SmartHomeSystem() {
         devices = new HashMap<>();
         scheduledTasks = new ArrayList<>();
         automatedTriggers = new ArrayList<>();
+        ids=new HashSet<>();
     }
-
+    
     public void addDevice(int id, String type) {
+    	if(ids.contains(id)) {
+    		 System.out.println("Already Exists!!enter a unique id:");
+    		 return;
+    	
+    	}
         Device device = DeviceFactory.createDevice(id, type);
+        ids.add(id);
         devices.put(id, device);
     }
 
@@ -43,6 +61,14 @@ public class SmartHomeSystem {
             device.turnOff();
         }
     }
+    public void toggleOnOff(int id,int value) {
+    	if(value==0) {
+    		turnOff(id);
+    	}
+    	else if(value==1) {
+    		turnOn(id);
+    	}
+    }
     
     public void scheduleDevice(int deviceId, String time, String action) {
         Device device = devices.get(deviceId);
@@ -51,11 +77,12 @@ public class SmartHomeSystem {
             scheduledTasks.add(task);
             scheduleTaskExecution(task);
         }
+        
     }
     
     private void scheduleTaskExecution(ScheduledTask task) {
         // Schedule the task to execute at the specified time
-        Timer timer = new Timer();
+        timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
@@ -83,29 +110,44 @@ public class SmartHomeSystem {
         
     }
     
-    public void addAutomatedTrigger(String type,String relation,int value, String action) {
+    public void addAutomatedTrigger(String type,String relation,int value, String action,int intervalInSeconds,int triggerId) {
     	int checkDeviceId=0;
     	for(int x:devices.keySet()) {
          	if(devices.get(x).getType().equals(type)) {
          		checkDeviceId=x;
          	}
          }
-    	AutomatedTrigger trigger = new AutomatedTrigger(type,relation,value, action,devices.get(checkDeviceId));
+    	AutomatedTrigger trigger = new AutomatedTrigger(type,relation,value, action,devices.get(checkDeviceId),triggerId,intervalInSeconds);
         automatedTriggers.add(trigger);
-        startAutomatedTriggerChecking();
+        startAutomatedTriggerChecking(intervalInSeconds,trigger);
     }
     
-    public void startAutomatedTriggerChecking() {
+    public void startAutomatedTriggerChecking(int intervalInSeconds,AutomatedTrigger trigger) {
         // Create a timer and schedule it to check triggers every 60 seconds
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
+    	timer = new Timer();
+    	TimerTask task=new TimerTask() {
             @Override
             public void run() {
                 checkAutomatedTriggers();
             }
-        }, 0, 60 * 1000); // Check triggers every 60 seconds
+        };
+        timer.scheduleAtFixedRate(task, 0, intervalInSeconds * 1000);
+        System.out.println("new timertask created with interval:"+intervalInSeconds);
+        trigger.setTimerTask(task);
     }
-
+    public void changeTriggerCheckingInterval(int targetTriggerId,int newInterval) {
+    	boolean cancelled=false;
+    	for(AutomatedTrigger x:automatedTriggers) {
+    		if(x.getTriggerId()==targetTriggerId) {
+    			cancelled=x.getTimerTask().cancel();
+    			x.setIntervalInSeconds(newInterval);
+    		}
+    		if(cancelled==true) {
+        		startAutomatedTriggerChecking(newInterval,x);
+        	}
+    	}
+    	
+    }
     public void checkAutomatedTriggers() {
         for (AutomatedTrigger trigger : automatedTriggers) {
             if (trigger.isTriggered()) {
@@ -113,6 +155,7 @@ public class SmartHomeSystem {
                 trigger.execute(devices);
             }
         }
+        
     }
     
     public void statusReport() {
@@ -136,8 +179,11 @@ public class SmartHomeSystem {
     public void automatedTriggers() {
     	System.out.print("[");
     	for(AutomatedTrigger x:automatedTriggers) {
-    	   System.out.print("{condition:"+x.getCondition()+", action: \""+x.getAction()+"\"}");
+    	   System.out.print("{  TriggerId: "+x.getTriggerId()+" condition:"+x.getCondition()+", action: \""+x.getAction()+"\" Time Interval:"+x.getIntervalInSeconds()+"}");
     	}
     	System.out.print("]");
     }
+    
+
 }
+
